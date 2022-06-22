20a82a2f62d5d8ac8f900bc873260797dc878dc7
beyoutoday <github.mail2116@gmail.com>
2022-06-22 13:52:46 -0700

    blocker drawn, and collides with coin

--- src/main.py
+++ src/main.py
@@ -13,6 +13,21 @@ def draw_coin(coins):
         pos_y = int(coin.body.position.y)
         pygame.draw.circle(screen, (0,0,0), (pos_x, pos_y), 80)
 
+
+def static_blocker(space):
+    body = pymunk.Body(body_type = pymunk.Body.STATIC)
+    body.position = (500, 500)
+    shape = pymunk.Circle(body, 50)
+    space.add(body, shape)
+    return shape
+
+def draw_static_blocker(blockers):
+     for b in blockers:
+        pos_x = int(b.body.position.x)
+        pos_y = int(b.body.position.y)
+        pygame.draw.circle(screen, (0,0,0), (pos_x, pos_y), 50)
+
+
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
@@ -20,6 +35,9 @@ space = pymunk.Space()
 space.gravity = (0, 100)
 coins = []
 coins.append(create_coin(space))
+
+blockers = []
+blockers.append(static_blocker(space))
 
 while True:
     for event in pygame.event.get():
@@ -29,6 +47,7 @@ while True:
 
     screen.fill((217, 217, 217)) # background color
     draw_coin(coins)
+    draw_static_blocker(blockers)
     space.step(1/50)
     pygame.display.update()
     clock.tick(120)
