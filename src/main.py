a28bb20b192139093faadab0f6f2e39366298695
beyoutoday <github.mail2116@gmail.com>
2022-06-22 14:14:02 -0700

    more blockers, mousebuttondown spawns more 'coins'

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import pygame, sys, pymunk 
 
-def create_coin(space):
+
+def create_coin(space, pos):
     body = pymunk.Body( 1, 100, body_type = pymunk.Body.DYNAMIC) 
-    body.position = (400, 0)
-    shape = pymunk.Circle(body, 80)
+    body.position = pos
+    shape = pymunk.Circle(body, 50)
     space.add(body, shape)
     return shape
 
@@ -11,12 +12,12 @@ def draw_coin(coins):
     for coin in coins:
         pos_x = int(coin.body.position.x)
         pos_y = int(coin.body.position.y)
-        pygame.draw.circle(screen, (0,0,0), (pos_x, pos_y), 80)
+        pygame.draw.circle(screen, (0,0,0), (pos_x, pos_y), 50)
 
 
-def static_blocker(space):
+def static_blocker(space, pos):
     body = pymunk.Body(body_type = pymunk.Body.STATIC)
-    body.position = (500, 500)
+    body.position = pos
     shape = pymunk.Circle(body, 50)
     space.add(body, shape)
     return shape
@@ -32,18 +33,26 @@ pygame.init()
 screen = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
 space = pymunk.Space()
-space.gravity = (0, 100)
+space.gravity = (0, 200)
 coins = []
-coins.append(create_coin(space))
 
 blockers = []
-blockers.append(static_blocker(space))
+blockers.append(static_blocker(space, (500, 500)))
+blockers.append(static_blocker(space, (250, 400)))
+blockers.append(static_blocker(space, (280, 700)))
+blockers.append(static_blocker(space, (880, 600)))
+blockers.append(static_blocker(space, (280, 700)))
+blockers.append(static_blocker(space, (580, 300)))
+
+blockers.append(static_blocker(space, (450, 230)))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            coins.append(create_coin(space, event.pos))
 
     screen.fill((217, 217, 217)) # background color
     draw_coin(coins)
